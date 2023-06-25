88d18643c9cb0edb29637b9cf3d735f1d3c1c21d
Eray Alkış <erayalkis@hotmail.com>
2023-06-25 19:57:37 +0300

    Create MachineVitals struct and refactor helper code

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,14 +1,28 @@
 use once_cell::sync::Lazy;
-use systemstat::{System, Platform};
+use systemstat::{System, Platform, ByteSize, Duration};
 
 static SYS: once_cell::sync::Lazy<systemstat::platform::linux::PlatformImpl> = Lazy::new(|| {System::new()});
 
+pub struct MachineVitals {
+  pub mem_free: ByteSize,
+  pub mem_total: ByteSize,
+  pub cpu_temp: f32,
+  pub uptime: Duration
+}
 
-pub fn get_vitals() -> String {
+pub fn get_vitals() -> MachineVitals {
 
   let mem = SYS.memory().unwrap();
+  let cpu_temp = SYS.cpu_temp().unwrap();
+  let uptime = SYS.uptime().unwrap();
 
-  let vitals_string = format!("Memory available: {}", mem.free);
 
-  return vitals_string;
+  let vitals = MachineVitals  {
+    mem_free: mem.free,
+    mem_total: mem.total,
+    cpu_temp,
+    uptime
+  };
+
+  return vitals;
 }
