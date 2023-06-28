2461a101f850330a9bb0df5f435e74a3a07ee676
Eray Alkış <lolpo12345@outlook.com>
2023-06-28 03:34:12 +0300

    Write code for getting servers and testing ports (WIP)

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,8 @@
 use once_cell::sync::Lazy;
 use sysinfo::{System, SystemExt, CpuExt};
 use std::sync::Mutex;
+use serde_yaml;
+use reqwest;
 
 static SYS: once_cell::sync::Lazy<Mutex<sysinfo::System>> = Lazy::new(|| {Mutex::new(System::new())});
 
@@ -9,6 +11,11 @@ pub struct MachineVitals {
   pub mem_free: u64,
   pub mem_used: u64,
   pub cpu_usage: f32
+}
+
+pub struct Server<'a> {
+  pub image: &'a str,
+  pub port: i64
 }
 
 pub fn get_vitals() -> MachineVitals {
@@ -30,4 +37,30 @@ pub fn get_vitals() -> MachineVitals {
   };
 
   return vitals;
+}
+
+pub fn get_servers() -> Vec<Server<'static>> {
+  let yaml_data = std::fs::File::open("containers.yml").unwrap();
+  let data: serde_yaml::Value = serde_yaml::from_reader(yaml_data).unwrap();
+
+  let servers = data.get("containers").unwrap().as_sequence().unwrap();
+  let mut servers_vec: Vec<Server> = Vec::new();
+
+  for server in servers {
+    let server_struct = Server {
+      image: server.get("image").unwrap().as_str().unwrap(),
+      port: server.get("port").unwrap().as_i64().unwrap()
+    };
+
+    servers_vec.push(server_struct);
+  }
+
+  return servers_vec;
+}
+
+pub fn probe_port(port: i64) -> bool {
+  let ip = format!("localhost:{}", port);
+  let result = reqwest::blocking::get(ip).unwrap();
+
+  result.status() == 200
 }
