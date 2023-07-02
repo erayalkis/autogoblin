f16614a15978a4f57eb5e4129f92c27a8a580123
Eray Alkış <lolpo12345@outlook.com>
2023-07-02 14:41:58 +0300

    Add network setup to setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -3,3 +3,9 @@
 source .env
 
 docker build -t autogoblin --build-arg DISCORD_TOKEN=$DISCORD_TOKEN .
+
+echo "Creating autogoblin network, please make sure you run all your other containers in this network."
+
+docker network create autogoblin_network
+
+echo "Setup complete!"
